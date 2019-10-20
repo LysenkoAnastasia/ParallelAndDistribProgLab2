90f01f2a69c7d87a24f5c945be2220e1203a0b4b
LysenkoAnastasia <lysenko2712@mail.ru>
2019-10-20 22:06:52 +0300

    Scripted auto-commit on change (2019-10-20 22:06:52) by gitwatch.sh

--- src/main/java/FlightsApp.java
+++ src/main/java/FlightsApp.java
@@ -1,16 +1,26 @@
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Int;
+import scala.Tuple2;
 
 import java.util.Arrays;
 
 public class FlightsApp {
     public static void main(String[] args) {
+        FlightParser flP = new FlightParser();
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaRDD<String> airportsFile = sc.textFile("L_AIRPORT_ID.csv");
+        JavaRDD<String> airportsFile = sc.textFile("L_AIRPORT_ID");
         JavaRDD<String> flightsFile = sc.textFile("664600583_T_ONTIME_sample.csv");
-        
 
+        JavaPairRDD<Integer, String> airportPair = airportsFile.mapToPair(s -> new Tuple2<>(flP.getAirportId(s), flP.getAiroportName(s))
+        );
+
+        JavaPairRDD<Tuple2<Integer, Integer>, Tuple2<Float, Float>> flightPair = flightsFile.mapToPair(s -> new Tuple2<>(new Tuple2<>(flP.getOrigionAirportID(s), flP.getDestAirportID(s)),
+                new Tuple2<>(flP.getDelayTime(s), flP.getCancelled(s)))
+        );
     }
+
 }
