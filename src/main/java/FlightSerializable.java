38b25ad915cc272dc8f9b076d71d86e9054b2329
LysenkoAnastasia <lysenko2712@mail.ru>
2019-10-20 22:37:35 +0300

    Scripted auto-commit on change (2019-10-20 22:37:35) by gitwatch.sh

--- src/main/java/FlightSerializable.java
+++ src/main/java/FlightSerializable.java
@@ -1,5 +1,32 @@
 import java.io.Serializable;
 
 public class FlightSerializable implements Serializable {
+    private int origionAirportID;
+    private int destAirportID;
+    private float delayTime;
+    private float cancelled;
+
+    public FlightSerializable(int origionAirportID, int destAirportID, float delayTime, float cancelled) {
+        this.origionAirportID = origionAirportID;
+        this.destAirportID = destAirportID;
+        this.delayTime = delayTime;
+        this.cancelled = cancelled;
+    }
+
+    public int getDestAirportID () {
+        return destAirportID;
+    }
+
+    public int getOrigionAirportID() {
+        return origionAirportID;
+    }
+
+    public float getDelayTime() {
+        return delayTime;
+    }
+
+    public float getCancelled() {
+        return cancelled;
+    }
 
 }
