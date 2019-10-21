1e3a05d5e59158083ede3eeed50a519297acfd7e
LysenkoAnastasia <lysenko2712@mail.ru>
2019-10-21 15:17:22 +0300

    Scripted auto-commit on change (2019-10-21 15:17:22) by gitwatch.sh

--- src/main/java/FlightsApp.java
+++ src/main/java/FlightsApp.java
@@ -9,12 +9,12 @@ public class FlightsApp {
         FlightParser flP = new FlightParser();
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaPairRDD<Integer, String> airportPair = sc.textFile("L_AIRPORT_ID").mapToPair(s -> new Tuple2<>(flP.getAirportId(s), flP.getAiroportName(s))
+        );
         
-        JavaRDD<String> airportsFile = sc.textFile("L_AIRPORT_ID");
         JavaRDD<String> flightsFile = sc.textFile("664600583_T_ONTIME_sample.csv");
 
-        JavaPairRDD<Integer, String> airportPair = airportsFile.mapToPair(s -> new Tuple2<>(flP.getAirportId(s), flP.getAiroportName(s))
-        );
 
         JavaPairRDD<Tuple2<Integer, Integer>, Tuple2<Float, Float>> flightPair = flightsFile.mapToPair(s -> new Tuple2<>(new Tuple2<>(flP.getOrigionAirportID(s), flP.getDestAirportID(s)),
                 new Tuple2<>(flP.getDelayTime(s), flP.getCancelled(s)))
