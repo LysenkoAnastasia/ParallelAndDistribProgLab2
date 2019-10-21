cb8194394cab6a134b9754ac89b82b6653517828
LysenkoAnastasia <lysenko2712@mail.ru>
2019-10-21 14:11:46 +0300

    Scripted auto-commit on change (2019-10-21 14:11:46) by gitwatch.sh

--- src/main/java/FlightsApp.java
+++ src/main/java/FlightsApp.java
@@ -2,10 +2,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import scala.Int;
 import scala.Tuple2;
-
-import java.util.Arrays;
 
 public class FlightsApp {
     public static void main(String[] args) {
