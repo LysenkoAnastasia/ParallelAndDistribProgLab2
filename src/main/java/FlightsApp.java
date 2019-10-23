65055f1262bdc15dd03d152b90ff1b0b7c6b8ad9
LysenkoAnastasia <lysenko2712@mail.ru>
2019-10-23 23:59:21 +0300

    Scripted auto-commit on change (2019-10-23 23:59:21) by gitwatch.sh

--- src/main/java/FlightsApp.java
+++ src/main/java/FlightsApp.java
@@ -13,7 +13,7 @@ public class FlightsApp {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        final Broadcast+<Map<Long, String>> airportsBroadcasted = sc.textFile("L_AIRPORT_ID").mapToPair(s -> new Tuple2<>(flP.getAirportId(s), flP.getAiroportName(s))
+        final Broadcast<Map<Long, String>> airportsBroadcasted = sc.textFile("L_AIRPORT_ID").mapToPair(s -> new Tuple2<>(flP.getAirportId(s), flP.getAiroportName(s))
         ).collectAsMap();
 
         JavaRDD<String> flightsFile = sc.textFile("664600583_T_ONTIME_sample.csv");
