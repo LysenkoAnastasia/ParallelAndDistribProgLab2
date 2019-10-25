e6fe3de33a11c682f8f482254306ec2e98522257
LysenkoAnastasia <lysenko2712@mail.ru>
2019-10-25 16:19:44 +0300

    Scripted auto-commit on change (2019-10-25 16:19:44) by gitwatch.sh

--- src/main/java/ru/bmstu/hadoop/spark/lab3/FlightsApp.java
+++ src/main/java/ru/bmstu/hadoop/spark/lab3/FlightsApp.java
@@ -6,7 +6,10 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.broadcast.Broadcast;
 import scala.Tuple2;
 
+import java.util.Iterator;
 import java.util.Map;
+
+import static org.spark_project.guava.primitives.Longs.max;
 
 
 public class FlightsApp {
@@ -39,13 +42,26 @@ public class FlightsApp {
 
         flightGroupPair.map(
                 elem -> {
-//                    FlightSerializable fs = elem._2().iterator().next()
+                    Iterator<FlightSerializable> fs = elem._2().iterator();
+                    Long cancelled = 0L, delTime, count = 0L, countDel = 0L;
+                    Long maxDelTime = Long.MIN_VALUE;
+                    while (fs.hasNext()) {
+                        FlightSerializable flNext = fs.next();
+                        delTime  = flNext.getDelayTime();
+                        if (delTime > 0) {
+                            countDel++;
+                        }
+                        maxDelTime = max(maxDelTime, delTime);
+                        cancelled += flNext.getCancelled();
+                        count ++;
+
+                    }
                     return new Tuple2<>(
                             new Tuple2<>(
                                     airportsBroadcasted.getValue().get(elem._1()._1()),
                                     airportsBroadcasted.getValue().get(elem._1()._2())
                             ),
-                            "result"
+                            maxDelTime + " " + countDel*100/count + " " + cancelled*100/count
                     );
                 }
         ).saveAsTextFile("output");
